622c91cbbf13dd8c4f5aa68fba1de305d0a70dc8
Artur Kozieł <arcczi@gmail.com>
2018-04-25 23:31:53 +0200

    working on reusable add/edit component

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: '',
+    amount
+  }
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({description})) //es6 shorthand operator
+  }
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({note}))
+  }
+
+  onAmountChange = (e) => {
+    const amount = e.target.value
+
+    if(amount.match(/^\d*(\.\d{0.2})?$/)) {
+      this.setState(() => ({amount}))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="Description" autoFocus value={this.state.description} onChange={this.onDescriptionChange}/>
+          <input type="text" placeholder="Amount" value={this.state.amount} onChange={this.onAmountChange}/>
+          <textarea placeholder="Add a note (optional)" value={this.state.note} onChange={this.onNoteChange}></textarea>
+          <button>Add expense</button>
+        </form>
+      </div>
+    )
+  }
+}
