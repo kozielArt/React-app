7942487ebee53e40357e98db616b8a8c2fa68822
Artur Kozieł <arcczi@gmail.com>
2018-05-11 14:08:34 +0200

    filter reducer tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,32 +1,31 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test('Should set up remoe expense action object ', () => {
-  const action = removeExpense({ id: '123' });
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
-    id: '123'
-  })
-})
+    id: '123abc'
+  });
+});
 
-test('should setup edit expense action object ', () => {
-  const action = editExpense('123', { note: 'note text' });
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', { note: 'New note value' });
   expect(action).toEqual({
     type: 'EDIT_EXPENSE',
-    id: '123',
+    id: '123abc',
     updates: {
-      note: 'note text'
+      note: 'New note value'
     }
-  })
-})
+  });
+});
 
 test('should setup add expense action object with provided values', () => {
   const expenseData = {
-    description: 'coffe',
-    amount: 2000,
+    description: 'Rent',
+    amount: 109500,
     createdAt: 1000,
-    note: 'note abc'
-  }
-
+    note: 'This was last months rent'
+  };
   const action = addExpense(expenseData);
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
@@ -34,23 +33,19 @@ test('should setup add expense action object with provided values', () => {
       ...expenseData,
       id: expect.any(String)
     }
-  })
-})
+  });
+});
 
 test('should setup add expense action object with default values', () => {
-  const expenseData = {
-    description: '',
-    amount: 0,
-    createdAt: 0,
-    note: ''
-  }
-
-  const action = addExpense(expenseData);
+  const action = addExpense();
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: {
-      ...expenseData,
-      id: expect.any(String)
+      id: expect.any(String),
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
     }
-  })
-})
+  });
+});
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,36 +1,35 @@
-import selectExpenses from '../../selectors/expenses';
 import moment from 'moment';
+import selectExpenses from '../../selectors/expenses';
+
 const expenses = [{
   id: '1',
-  description: 'Cola',
+  description: 'Gum',
   note: '',
-  amount: 100,
+  amount: 195,
   createdAt: 0
-},
-{
+}, {
   id: '2',
-  description: 'Ice cream',
+  description: 'Rent',
   note: '',
-  amount: 300,
+  amount: 109500,
   createdAt: moment(0).subtract(4, 'days').valueOf()
-},
-{
+}, {
   id: '3',
-  description: 'Bread',
+  description: 'Credit Card',
   note: '',
-  amount: 600,
-  createdAt:moment(0).add(4, 'days').valueOf()
-}]
+  amount: 4500,
+  createdAt: moment(0).add(4, 'days').valueOf()
+}];
 
-test('should filter by text value ', () => {
+test('should filter by text value', () => {
   const filters = {
     text: 'e',
     sortBy: 'date',
     startDate: undefined,
     endDate: undefined
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[2], expenses[1]])
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[1]]);
 });
 
 test('should filter by startDate', () => {
@@ -39,32 +38,21 @@ test('should filter by startDate', () => {
     sortBy: 'date',
     startDate: moment(0),
     endDate: undefined
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[2], expenses[0]])
-})
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0]]);
+});
 
-test('should filer by end date', () => {
+test('should filter by endDate', () => {
   const filters = {
     text: '',
     sortBy: 'date',
     startDate: undefined,
     endDate: moment(0).add(2, 'days')
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[0], expenses[1]])
-})
-
-test('should filer by start date', () => {
-  const filters = {
-    text: '',
-    sortBy: 'date',
-    startDate: moment(0).add(2, 'days'),
-    endDate: undefined
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[2]])
-})
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
 
 test('should sort by date', () => {
   const filters = {
@@ -72,10 +60,10 @@ test('should sort by date', () => {
     sortBy: 'date',
     startDate: undefined,
     endDate: undefined
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[2], expenses[0], expenses[1]])
-})
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
 
 test('should sort by amount', () => {
   const filters = {
@@ -83,8 +71,7 @@ test('should sort by amount', () => {
     sortBy: 'amount',
     startDate: undefined,
     endDate: undefined
-  }
-  const result = selectExpenses(expenses, filters)
-  expect(result).toEqual([expenses[2], expenses[1], expenses[0]])
-})
-
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
